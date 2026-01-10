30d783d5f1b6e4ed58738138dfa7af9566ea999a
Kesang Lama <kesanghyolmo288@gmail.com>
2026-01-11 02:07:24 +0545

    sitemap bug fixed

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const SITE_URL = "https://hyolmohelpinghands.com.np";
+const SITE_URL = "https://www.hyolmohelpinghands.org.np";
 
 export default function robots(): MetadataRoute.Robots {
   return {
